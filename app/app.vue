bf25a1f260345fc9cb7d091c103ad3cfc8c01604
AndikaWahyuSyaputra <78039794+MartinZakhaev@users.noreply.github.com>
2026-07-04 18:06:01 +0700

    feat: add superadmin application shell

---
+++ app/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <NuxtPage />
+</template>
+
+<style>
+:root {
+  color-scheme: light;
+  font-family: Inter, ui-sans-serif, system-ui, sans-serif;
+  background: #f7f7f8;
+  color: #18181b;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+</style>
